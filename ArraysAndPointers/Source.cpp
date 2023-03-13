3ce97afbe886c070e395b506d91074bd463f2424
lennart@familie-richter.de <lennart@familie-richter.de>
2023-03-13 14:59:36 -0700

    Added dynamic array sizing using new and delete

--- ArraysAndPointers/Source.cpp
+++ ArraysAndPointers/Source.cpp
@@ -1,23 +1,36 @@
 #include <iostream>
-void elementAndAddress(const int nums[], int size);
-void elementAndAddressWithPointer(const int nums[]);
+void elementAndAddress(const int nums[], const int size);
+void elementAndAddressWithPointer(const int nums[], const int size);
 
 int main() {
-	int nums[] = { 1,2,3,4,5,6,7,8,9,10 };
-	elementAndAddress(nums, 10);
-	elementAndAddressWithPointer(nums);
+	//int nums[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int size;
+	std::cout << "Enter the size of your array: ";
+	std::cin >> size;
+	int* nums = new int[size];
 
+	for (int i = 0; i < size; i++) {
+		int temp;
+		std::cin >> temp;
+		nums[i] = temp;
+	}
+
+
+
+	elementAndAddress(nums, size);
+	elementAndAddressWithPointer(nums, size);
+	delete[] nums;
 }
 
-void elementAndAddress(const int nums[], int size) {
+void elementAndAddress(const int nums[], const int size) {
 	for (int i = 0; i < size; i++) {
 		std::cout << "VAL:" << nums[i] << "ADR:" << &nums[i] << std::endl;
 	}
 }
 
-void elementAndAddressWithPointer(const int nums[]) {
+void elementAndAddressWithPointer(const int nums[], const int size) {
 	int* begPtr = (int*)&nums[0];
-	int* endPtr = (int*)&nums[9];
+	int* endPtr = (int*)&nums[size - 1];
 	while (begPtr <= endPtr) {
 		std::cout << "VAL:" << *begPtr << "ADR:" << &begPtr << std::endl;
 		begPtr++;
